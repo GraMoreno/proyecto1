52c9c8e8fa619bd4270f657b049a46a0059a8183
Graciela <gmorenoing@gmail.com>
2018-11-13 15:50:01 -0300

    creacion inicial de controlador de consultas

---
+++ servidor/controladores/controlador-consultas.js
@@ -0,0 +1,28 @@
+//importamos los modelos de moongose para acceder a sus métodos
+var Usuario = require("../models/usuario");
+var Producto = require("../models/producto");
+var Consulta= require("../models/consultas-usuario");
+
+let listarConsultas = (req,res)=> {
+    let usuario = Usuario.findById(req.params.id);
+    let consulta = Consulta.find({producto:req.params.id});
+
+    //ejecutamos el metodo find para obtener los usuarios
+    Producto.find()
+    .then ((consulta)=>{
+        return res.send(usuario)
+    })
+    .catch((err)=>{
+        return res.send(err)
+    })
+}
+
+let crearConsulta=(req,res)=>{
+    Consulta.create(req.body)
+    .then((consultaCreada)=>{
+        return res.send({mensaje:"Consulta creada",detalle:consultaCreada})
+    })
+    .catch((errorCreando)=>{
+        return res.send({mensaje:"No se pudo crear", error:errorCreando})
+    })
+}
