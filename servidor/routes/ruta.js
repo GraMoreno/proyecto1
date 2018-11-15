6fbb79b76752a35632879e7dfbb4a4aa42a57c66
Graciela <gmorenoing@gmail.com>
2018-11-15 11:59:47 -0300

    añadi ruta consulta de usuario

--- servidor/routes/ruta.js
+++ servidor/routes/ruta.js
@@ -5,8 +5,12 @@ const router = express.Router();
 let catController = require('../controladores/controlador-categorias');
 //Importamos el controlador del elemento producto
 let prodController = require('../controladores/controlador-productos');
+//Importamos el controlador del elemento usuarios
+let userController= require('../controladores/controlador-usuarios');
+//Importamos el controlador del elemento consultas del usuario
+let consuController = require('../controladores/controlador-consultas');
 
-//ahora se definen las rutas
+//ahora se definen las rutas categorias
 router.get('/',catController.listarCategorias);
 router.post('/',catController.crearCategoria);
 router.get('/categorias/:id',catController.verCatergoria);
@@ -19,6 +23,16 @@ router.get('/productos',prodController.listarProductos);
 router.post('/productos',prodController.crearProducto);
 router.get('/productos/:id',prodController.verProducto);
 router.get('/productos/:id',prodController.eliminarProducto);
+
+//rutas de usuario
+router.get('/usuarios',userController.listarUsuarios);
+router.post('/usuarios',userController.crearUsuario);
+router.get('/usuarios/:id',userController.verUsuario);
+
+//rutas de consultas del usuario
+router.get ('/consultasUsuario',consuController.listarConsultas);
+router.post ('/consultaUsuario',consuController.crearConsulta);
+
 
 //router.get('/', (req, res)=>{
 //    res.send('Hello world');
