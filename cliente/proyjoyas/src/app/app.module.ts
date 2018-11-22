d7ae515d9aa4d3d4b4a63fa188d6522434f63bc9
Graciela <gmorenoing@gmail.com>
2018-11-22 12:02:59 -0300

    modificacion de appmodule

--- cliente/proyjoyas/src/app/app.module.ts
+++ cliente/proyjoyas/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsuariosComponent } from './Components/usuarios/usuarios.component';
+import { ConsultasComponent } from './Components/consultas/consultas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent,
+    ConsultasComponent
   ],
   imports: [
     BrowserModule
