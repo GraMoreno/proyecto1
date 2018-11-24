0e484f35ce9847c528b1e30651e9613caa871120
Graciela <gmorenoing@gmail.com>
2018-11-24 09:45:57 -0300

    creacion rutas para el usuario

---
+++ servidor/routes/usuario.js
@@ -0,0 +1,18 @@
+//Creamos una instancia de express
+var express = require('express');
+//Importamos el controlador de usuario
+var UserController = require('../controladores/usuario');
+
+//Utilizamos el modulo router de Express
+var router = express.Router();
+// Importamos el middleware para manejar authenticacion
+var md_auth = require ('../middlewares/auth');
+
+// Rutas de la App
+router.get('/',UserController.obtenerUsuarios);
+router.post('/registro',UserController.crearUsuario);
+router.post('/login',UserController.login);
+router.get('/prueba',md_auth,UserController.prueba);
+
+// Lo exportamos para usar en app.js
+module.exports = router;
