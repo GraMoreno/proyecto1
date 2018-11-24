c3fb684e74c59a3809452c0eb29ac16a0f0c99e7
Graciela <gmorenoing@gmail.com>
2018-11-24 09:25:56 -0300

    cambios en controlador de usuario para el login

--- servidor/controladores/controlador-usuarios.js
+++ servidor/controladores/controlador-usuarios.js
@@ -1,61 +1,110 @@
 //importamos los modelos de moongose para acceder a sus métodos
 var Usuario = require("../models/usuario");
+var bcrypt = require('bcrypt');
+//definimos la cantidad de repeticiones
+const SaltRounds = 10;
 
-let listarUsuarios = (req,res)=> {
-    //ejecutamos el metodo find para obtener los usuarios
-    Usuario.find()
-    .then ((Usuario)=>{
-        return res.send(Usuario)
-    })
-    .catch((err)=>{
-        return res.send(err)
-    })
-}
+// importar servicio jwt
+var jwt = require('../services/jwt');
 
-let crearUsuario = (req,res)=>{
-    //crear la variable de usuario
-    Usuario.create(req.body).then((usuarioCreado)=>{
-        return res.send({mensaje:"Usuario Creado", detalles: usuarioCreado})
-    })
-    .catch((errorCreando)=>{
-        return res.send({mensaje:"No se puede crear", error: errorCreando})
-    })
-}
-let actualizarUsuario = (req,res)=>{
-    Usuario.findByIdAndUpdate(req.params.id,req.body)
-    .then((usuarioActualizado)=>{
-        return res.send({mensaje:"Actualizado"})
-    })
-    .catch((err)=>{
-        return res.send({mensaje:err})
+// funcion que se ejecutara desde la ruta
+function prueba (req,res){
+    res.status(200).send({
+        message:'Estas logueado'
     })
 }
 
-let verUsuario=(req,res)=>{
-    Usuario.findById(req.params.id)
-    .then((Usuario)=>{
-        return res.send(Usuario)
+let crearUsuario = (req, res) => {
+    var usuario = new Usuario()
+    var body = req.body
+    console.log(body)
+    if(!body.nombre || !body.email){
+      return res.send({message:"Introduce email y nombre"})
+    }
+    // Tomamos los valores desde los body del body
+    usuario.nombre = body.nombre
+    usuario.email = body.email
+    // Verificamos que la constraseña se haya enviado en request,
+    // caso contrario devolvemos un mensaje de error
+    if (!body.password) {
+      return res.status(202).send({ mensaje: 'Introduce contraseña' })
+    }
+    // Si se recibio encriptamos contraseña
+    usuario.password = bcrypt.hashSync(body.password, saltRounds);
+    usuario.save().then((usuarioGuardado)=>{
+      return res.send({usuario:usuarioGuardado,token:jwt.createToken(usuarioGuardado)})
     })
-    .catch((err)=>{
-        return res.send({mensaje:"No se pudo encontrar el usuario"})
-    })
-}
+  }
+  
+  // loguear usuario
+  
+  let login = (req, res) => {
+    // Recibimos los parametros del body con body-parser
+    let body = req.body
+  
+    let email = body.email
+    let password = body.password
+  // Buscamos un usuario en base al email que recibimos del body
+    Usuario.findOne({email: email.toLowerCase()})
+    // si se realiza la busqueda se ejecuta un then
+    // Posteriormente, devolverá un false si no se encontró usuario
+    .then((usuarioEncontrado)=>{
+      // Si no se encontro usuario (usuarioEncontrado == false)
+      if (!usuarioEncontrado){
+        return res.send({mensaje:"No existe usuario"})
+      }
+      // Sino, continuamos
+      // Comparamos la contraseña enviada en el body con la que tenemos guardada en base de datos (ambas encriptadas)
+      bcrypt.compare(password,usuarioEncontrado.password)
+      .then((passOk)=>{
+        if(passOk){
+          // Si salió todo bien, enviamos el JWT del usuario, que le permitirá loguearse
+          return res.status(200).send({
+            token: jwt.createToken(usuarioEncontrado)
+          })
+        }else{
+          // Sino, la contraseña no es correcta
+          return res.status(400).send({message:"Contraseña incorrecta"})
+        }
+      })
+    });
+  }
+  
 
-let eliminarUsuario=(req,res)=>{
-    Usuario.findByIdAndRemove(req.params.id)
-    .then(()=>{
-        return res.send({mensaje:"El Usuario se elimino correctamente"})
+  let actualizarUsuario = (req, res) => {
+    // req.param se refiere al parametro de url
+    var userId = req.params.id
+    // req.body al cuerpo de la petición
+    var update = req.body
+  
+    Usuario.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+      if (err) {
+        res.status(500).send({ message: 'No se pudo actualizar el usuario' })
+      } else {
+        if (!userUpdated) {
+          res.status(404).send('No se pudo actualizar')
+        } else {
+          res.status(200).send({ user: userUpdated })
+        }
+      }
     })
-    .catch((error)=>{
-        return  res.send({mensaje:"No se puedo eliminar el usuario"})
+  }
+
+  let obtenerUsuarios = (req, res) => {
+    Usuario.find({}, 'name surname email', (err, users) => {
+      if (err) {
+        return err
+      } else {
+        res.status(200).send(users)
+      }
     })
-}
+  }
 
 //exportar los modulos
-module.exports={
-    listarUsuarios,
+module.exports = {
     crearUsuario,
-    verUsuario,
+    login,
     actualizarUsuario,
-    eliminarUsuario,
-}
+    obtenerUsuarios,
+    prueba
+  }
