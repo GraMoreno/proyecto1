5badaf88d858eb268748d63ee079b7e0047ee607
Graciela <gmorenoing@gmail.com>
2018-11-29 14:30:59 -0300

    cambios en achivo jwt

--- servidor/services/jwt.js
+++ servidor/services/jwt.js
@@ -1,16 +1,17 @@
-//servicio para manejar JWT en la app
-"use strict";
+// Servicio para manejar JWTS en la app
 
-var jwt = require("jwt-simple")
-var secret="ClaveSecreta";
-// funcion que recibe un usuario para generar el JWT
+'use strict'
+
+var jwt = require("jwt-simple");
+var secret = "claveSecreta";
+//Creamos una funcion que recibe un usuario para generar el JWT
 exports.createToken = user => {
-    // Definimos estrucutra del token
-    let payload = {
-      name: user.name,
-      email: user.email,
-    };
-  
-    //Devolvemos el token encodificado
-    return jwt.encode(payload, secret);
+  // Definimos estrucutra del token
+  let payload = {
+    name: user.name,
+    email: user.email,
   };
+
+  //Devolvemos el token encodificado
+  return jwt.encode(payload, secret);
+};
