63df14a7fc9b6af12a747d040ca4e07f3a4910c9
Graciela <gmorenoing@gmail.com>
2018-11-29 14:34:08 -0300

    modificacon en appmodule

--- cliente/proyjoyas/src/app/app.module.ts
+++ cliente/proyjoyas/src/app/app.module.ts
@@ -4,17 +4,44 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './Components/usuarios/usuarios.component';
 import { ConsultasComponent } from './Components/consultas/consultas.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CrearUserComponent } from './pages/crear-user/crear-user.component';
+
+
+import { Routes, CanActivate, RouterModule } from '@angular/router';
+import { UsuarioService} from './services/usuario.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { VerificarLogueadoService } from './services/verificar-logueado.service';
+
+export const RUTAS: Routes = [
+ { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'crearusuario', component: CrearUserComponent },
+  //{ path: 'perfil', component: MiPerfilComponent, canActivate: [VerificarLogueadoService] },
+  //{ path: 'publicar', component: AgregarEntradaComponent, canActivate: [VerificarLogueadoService] },
+  //{ path: 'editar', component: EditarEntradaComponent, canActivate: [VerificarLogueadoService] },
+  { path: '**', redirectTo: '' },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UsuariosComponent,
-    ConsultasComponent
+    ConsultasComponent,
+    HomeComponent,
+    LoginComponent,
+    CrearUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(RUTAS),
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [VerificarLogueadoService, UsuarioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
