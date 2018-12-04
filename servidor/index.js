93ba4d0f38ec3101737b85e9d784b48c5f69588d
Graciela <gmorenoing@gmail.com>
2018-12-04 15:25:09 -0300

    añadi cors algunos cambios para que se ejecute

--- servidor/index.js
+++ servidor/index.js
@@ -3,92 +3,52 @@ const path= require('path');
 var express = require ('express');
 //inicio de express
 var app = express();
+const cors = require('cors');
 
 // requiero mongoose
 const mongoose= require('mongoose');
 
 // importar body parser
-const bodyparser = require ('body-parser');
+const bodyParser = require ('body-parser');
 // para hacer las peticiones Posts
-app.use(bodyparser.urlencoded({extended:true}));
-app.use(bodyparser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// Configuramos cabeceras HTTP
-// Esto permitirá recibir peticiones de cualquier origen (nos servirá cuando utilicemos Angular)
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization" //to give access to all the headers provided
-  );
-  if(req.method === 'OPTIONS'){
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
-      return res.status(200).json({});
+let puerto = 3000;
+let direccionBaseDatos = 'mongodb://localhost:27017/proyjoyeria';
+//connecting databases
+mongoose.connect(direccionBaseDatos, {useNewUrlParser:true}, (err,res)=>{
+  if (err) { throw err;}
+  else {
+    console.log("se realizó la conexión a la base de datos" + direccionBaseDatos);
   }
-  next();
+  // empezamos a escuchar el puerto
+  app.listen(process.env.PORT || puerto, ()=>{
+    console.log("servidor corriendo en el puerto:" + puerto);
+  });
 });
-
-
-  let puerto = 3000;
-
-  //connecting databases
-let direccionBaseDatos ='mongodb://localhost/proyjoyeria';
-//let direccionBaseDatos = "mongodb://meanblog:meanblog1234@ds237832.mlab.com:37832/meanblog"; 
-
-//mongoose.connect('mongodb://localhost/proyjoyeria')
 //.then(db => console.log('base de datos conectada'))
 //.catch(err => console.log(err));
 
-mongoose.connect(
-    direccionBaseDatos,
-    { useNewUrlParser: true },
-    (err, res) => {
-      if (err) {
-        throw err;
-      } else {
-        console.log(
-          "Se realizó la correción a la base de datos" + direccionBaseDatos
-        );
-        //Empezamos a escuchar el puerto
-        app.listen(process.env.PORT || puerto, () => {
-          console.log("Servidor corriendo en puerto: " + puerto);
-        });
-    }
-  }
-);
-
-
-
-//middlewares
-//para entender los datos que envia un formulario
-app.use(express.urlencoded({extended:false}));
-
-//setting
-// app.set('port', process.env.PORT || 3000);
-
-let mensaje ="estoy en hola";
-app.get ('/hola', (req,res)=>{
-    res.send({mensaje});
-})
-
-
 //importando rutas
 const indexRouter = require('./routes/ruta');
-//const usuarioRouter = require('./routes/usuario');
-//const categoriaRouter = require('./routes/categoria');
-//const productoRouter = require('./routes/producto');
 
+//middlewares
+app.use(cors({origin:'http://localhost:4200'}));
+
+//setting
+//app.set('port', process.env.PORT || puerto);
+
+//let mensaje ="estoy en hola";
+//app.get ('/hola', (req,res)=>{
+//    res.send({mensaje});
+//})
 
 //routers
-
-app.use(indexRouter);
-//app.use('/api',usuarioRouter);
-//app.use(usuarioRouter);
-//app.use(categoriaRouter);
-//app.use(productoRouter);
-
+app.use('/',indexRouter);
 
 //starting the server
 //app.listen(app.get('port'),()=>{
-//    console.log(`Server on port ${app.get('port')}`);
-//}
+ //   console.log(`Server on port ${app.get('port')}`);
+//})
+
