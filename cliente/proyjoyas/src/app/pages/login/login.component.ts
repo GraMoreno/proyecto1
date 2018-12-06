bd95a8d0dd27eb0b1da70eb714ee5de514839417
Graciela <gmorenoing@gmail.com>
2018-12-06 14:54:09 -0300

    cambios para probar login

--- cliente/proyjoyas/src/app/pages/login/login.component.ts
+++ cliente/proyjoyas/src/app/pages/login/login.component.ts
@@ -8,22 +8,22 @@ import { UsuarioService } from '../../services/usuario.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public nombre: string;
-  public email: string;
-  public password: string;
+  //public nombre: string;
+  public email: String;
+  public password: String;
   public logged;
 
   constructor ( public userSv: UsuarioService, public router: Router ) { }
-
+  
   ngOnInit() {
   }
-
+  
   probarLogin() {
-    this.userSv.login(this.nombre, this.email, this.password)
+    this.userSv.login(this.email, this.password)
     .then((ok) => {
       console.log('Ingresó correctamente');
       this.router.navigateByUrl('perfil');
     });
-  }
+    }
 
 }
